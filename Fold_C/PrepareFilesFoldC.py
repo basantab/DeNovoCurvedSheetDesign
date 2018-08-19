867b20daeacd90e8e1ff76d822b862521eed2282
Benjamin Basanta <basantab@uw.edu>
2018-08-19 07:48:35 -0700

    Updated fold C

--- Fold_C/PrepareFilesFoldC.py
+++ Fold_C/PrepareFilesFoldC.py
@@ -4,15 +4,17 @@
 #Length: xxx
 ""
 # USE: python build_blueprints.v2.py -xml template_bb+design.xml -blueresfile
+import sys
+sys.path.append("../")
+
 import itertools
 from argparse import ArgumentParser
 import re
-import sys
 import os
 import copy
 from Blueprint import Blueprint
 import numpy as np
-from BPBf_av_mod import *
+from BPB_functions import *
 import subprocess
 
 #==============================
@@ -33,7 +35,7 @@ motif_filename = args.motif
 abego = args.abego # for L3
 #topol = "L[1-1]H[15-15]L[2-2]H[7-7]L[5-5]E[4-4]L[2-2]E[4-4]L[1-1]H[10-15]L[1-2]E[11-11]L[2-2]E[10-10]L[2-2]E[12-12]L[2-2]E[10-10]L[1-1]"
 #topol = "L[1-1]H[18-18]L[1-2]H[6-6]L[5-5]E[4-4]L[2-2]E[4-4]L[1-1]H[11,13]L[2-3]E[11-11]L[2-2]E[10-10]L[2-2]E[12-12]L[2-2]E[10-10]L[1-1]"
-topol = "L[1-1]H[18-18]L[2-2]H[7-7]L[5-5]E[4-4]L[2-2]E[4-4]L[1-1]H[14-14]L[2-2]E[12-12]L[2-2]E[12-12]L[2-2]E[14-14]L[2-2]E[9-9]L[1-1]" # Original H3 length: 14
+topol = "L[1-1]H[16-16]L[2-2]H[7-7]L[5-5]E[4-4]L[2-2]E[4-4]L[1-1]H[14-14]L[2-2]E[10-10]L[2-2]E[10-10]L[2-2]E[12-12]L[2-2]E[9-9]L[1-1]" # Original H3 length: 14
 
 if motif_filename:
 
@@ -44,13 +46,13 @@ else:
 
 print topol
 #==============================
-# ncenter: Bulge N-ter is free of restrictions 
+# ncenter: Bulge N-ter is free of restrictions
 
 # ccenter: Bulge C-ter is free of restrictions
 
 
 
-bulges =  {'E3':7,'E6':5} # Specific definition. Bulges are located at positions defined by the user
+bulges =  {'E3':5,'E6':5} # Specific definition. Bulges are located at positions defined by the user
 
 
 # Make directory and bluprints for each combination along with bulge positions
@@ -60,7 +62,7 @@ for comb in combinations:
 	# Make the directory name
 	filename = 'r0-all_rev-' ; strand=0 ; resnum=0
 	for i,s in enumerate(ss):
-		filename+='%s%i' %(ss[i],comb[i])		
+		filename+='%s%i' %(ss[i],comb[i])
 
 	# First build a plain blueprint file to after creat a bp object to make easier the bulge combinations
 
@@ -75,7 +77,7 @@ for comb in combinations:
 		for seg in blue.segments:
 			if seg.id == key:
 				st_len = len(seg.bp_data)
-				if bulges[key] == 'n-center':		
+				if bulges[key] == 'n-center':
 					if st_len %2 == 0: # even strand length. Bulge must be at odd position
 						bposs = range(1,st_len,2)[1:-1]
 					else:
@@ -110,19 +112,19 @@ for comb in combinations:
 			#os.system('cp ../bp1 .')
 
 			## Build blueprints
-			MakeRefBlueprint(ss,comb,picked_bulges,refblue = 'bp')			
+			MakeRefBlueprint(ss,comb,picked_bulges,refblue = 'bp')
 			# split blueprint according to different phases in backbone generation
 			# pairing according to current blueprint
 			###########
 			# ATTENTION!!! to make more efficient the biasing with the SSPAIR
-			# headers of blueprints it is better only write the pairing that is 
+			# headers of blueprints it is better only write the pairing that is
 			# actually built with this blueprint, then 100% of biasing is focused on the region being constructed.
 			# Pairings must be indexed according to the current blueprint not the global one in contrast to segments
 			#########
 			# step1
 			MakeFirstBlueprint(refblue = 'bp', segments = ['E4','L8','E5'], \
 					   newblue = 'bp1', ss_pairing={'E1':['E2.A']},adapt={'E5':'E4'},specific_abego={'L2':[(0,'G'),(1,'G')]})
-			
+
 			# step2
 			AddSegmentToBlueprint(refblue = 'bp', segments = ['E3','L7'], blue0 = 'bp1', newblue='bp2', \
 						      append=False, ss_pairing_shift="SSPAIR 1-2.A.1", ss_pairing={'E1':['E2.A'],'E2':['E3.A']})
@@ -182,8 +184,8 @@ for comb in combinations:
                         # For a C-bulged hairpin
                         bulged_strand = 'E6'
                         seg = blue.segment_dict[bulged_strand]
-                        bulge2_shift = bulges[bulged_strand]  
-			 
+                        bulge2_shift = bulges[bulged_strand]
+
 			#----------------------
 			# Write flags file
 			#----------------------
@@ -195,11 +197,11 @@ for comb in combinations:
 			fileout = open('cst1','w') # E2//E3
 			fileout_min = open('cst1_min','w') # E2//E3
                         #st11 = RegularStrandCurvature(level=3,blueprint=blue,strand='E1',\
-                        #global_twist=40.0, global_twist_tol=30.0 )			
+                        #global_twist=40.0, global_twist_tol=30.0 )
                         #st22 = RegularStrandCurvature(level=3,blueprint=blue,strand='E2',\
                         #global_twist=50.0, global_twist_tol=35.0 )
 
-			
+
                         st1 = RegularStrandCurvature(level=2,blueprint=blue,strand='E1',\
                         global_bend=55.0,global_bend_tol=10.0,\
 			global_twist=35.0,global_twist_tol=10.0,\
@@ -220,20 +222,20 @@ for comb in combinations:
 			# Original: 70 twist, tol 5.0
 			'''
 			# Vars for Stage #1
-			SS_s1 = "-parser:script_vars s1_SS=%s\n" %(subprocess.check_output([ "bash", "/work/basantab/scripts/ss_from_bp.sh", "bp1"])) ; flags_out.write(SS_s1)
-			bp1_name = "-parser:script_vars bp1=../bp1\n" ; flags_out.write(bp1_name)
+			SS_s1 = "-parser:script_vars s1_SS=%s\n" %(''.join([ pos[2][0] for pos in Blueprint('bp1').bp_data])) ; flags_out.write(SS_s1)
+			bp1_name = "-parser:script_vars bp1=../bp1.b\n" ; flags_out.write(bp1_name)
 			bp1b_name = "-parser:script_vars bp1.b=../bp1.b\n" ; flags_out.write(bp1b_name)
 			bp1 = Blueprint('bp1'); r_res = [ i+1 for i,pos in enumerate(bp1.bp_data) if pos[-1] == 'R' ]
 			R_in_bp1= "-parser:script_vars R_in_bp1=%d-%d\n"%(r_res[0],r_res[-1]) ; flags_out.write(R_in_bp1)
 			cst1_name = "-parser:script_vars cst1=../cst1\n" ; flags_out.write(cst1_name)
 			cst1_min_name = "-parser:script_vars cst1_min=../cst1_min\n" ; flags_out.write(cst1_min_name)
-			
-			sthb = HbondsRegularHairpin(strand1="E1",strand2="E2",blueprint=blue) ; fileout.write(sthb)			
+
+			sthb = HbondsRegularHairpin(strand1="E1",strand2="E2",blueprint=blue) ; fileout.write(sthb)
 
 			fileout.write(st1) ; fileout.write(st2) # ; fileout.write(st11) ; fileout.write(st22)
 			fileout_min.write(st1) ; fileout_min.write(st2)
 
-			fileout.close()	
+			fileout.close()
 
 			#############################################
 
@@ -242,20 +244,20 @@ for comb in combinations:
 
 			# bulge constraints
 			blue = Blueprint('bp2.b') ; blue.reindex_blueprint(start=1)
-			s1 = blue.segment_dict['E1']	
+			s1 = blue.segment_dict['E1']
 			s2 = blue.segment_dict['E2']
-			
+
 			st = HbondsBulgedStrand(strand1="E1",strand2="E2",blueprint=blue) ; fileout.write(st)
 			#st = BulgedStrandCurvature(strand="E1",bend2=50,bend2_tol=5.0,blueprint=blue,constraint_type='bounded') ; fileout.write(st)
-			
+
                         # Specifiy movemap for minimization in XML
                         pos1 = s1.bp_data[0]
                         XMLReplaceXXXYYY(xml_lines=xml_lines,identifier='MoveMap2 Flexible',xxx=1, yyy=s2.bp_data[0][0]-1)
                         XMLReplaceXXXYYY(xml_lines=xml_lines,identifier='MoveMap2 Rigid',xxx=s2.bp_data[0][0], yyy=blue.bp_data[-1][0])
                         fileout.close()
-			
+
 			# Vars for Stage #2
-			SS_s2 = "-parser:script_vars s2_SS=%s\n" %(subprocess.check_output([ "bash", "/work/basantab/scripts/ss_from_bp.sh", "bp2"])) ; flags_out.write(SS_s2)
+			SS_s2 = "-parser:script_vars s2_SS=%s\n" %(''.join([ pos[2][0] for pos in Blueprint('bp2').bp_data])) ; flags_out.write(SS_s2)
 			bp2_name = "-parser:script_vars bp2=../bp2\n" ; flags_out.write(bp2_name)
 			bp2b_name = "-parser:script_vars bp2.b=../bp2.b\n" ; flags_out.write(bp2b_name)
                         bp2 = Blueprint('bp2'); r_res = [ i+1 for i,pos in enumerate(bp2.bp_data) if pos[-1] == 'R' ]
@@ -272,12 +274,12 @@ for comb in combinations:
                         s1 = blue.segment_dict['E1']
                         s2 = blue.segment_dict['E2']
                         s3 = blue.segment_dict['E3']
-                        s4 = blue.segment_dict['E4']	
+                        s4 = blue.segment_dict['E4']
 
 			# hbonds
 			st = HbondsBulgedStrand(strand1="E3",strand2="E4",blueprint=blue) ; fileout.write(st)
 			st = HbondsRegularHairpin(strand1="E2",strand2="E3",blueprint=blue) ; fileout.write(st)
-		
+
 
 			# csts of bulged E4
 			#st = BulgedStrandCurvature(strand="E4",bend1=60.0,bend1_tol=20.0, blueprint=blue,constraint_type='bounded')  ; fileout.write(st)
@@ -311,15 +313,15 @@ for comb in combinations:
                         XMLReplaceXXXYYY(xml_lines=xml_lines,identifier='MoveMap3 Flexible',xxx=s2.bp_data[-1][0], yyy=blue.bp_data[-1][0])
 
                         fileout.close()
-			
+
 			# Vars for Stage #3
-			SS_s3 = "-parser:script_vars s3_SS=%s\n" %(subprocess.check_output([ "bash", "/work/basantab/scripts/ss_from_bp.sh", "bp3"])) ; flags_out.write(SS_s3)
+			SS_s3 = "-parser:script_vars s3_SS=%s\n" %(''.join([ pos[2][0] for pos in Blueprint('bp3').bp_data])) ; flags_out.write(SS_s3)
 			bp3_name = "-parser:script_vars bp3=../bp3\n" ; flags_out.write(bp3_name)
 			bp3b_name = "-parser:script_vars bp3.b=../bp3.b\n" ; flags_out.write(bp3b_name)
 			bp3 = Blueprint('bp3'); r_res = [ i+1 for i,pos in enumerate(bp3.bp_data) if pos[-1] == 'R' ]
 			R_in_bp3= "-parser:script_vars R_in_bp3=%d-%d\n"%(r_res[0],r_res[-1]) ; flags_out.write(R_in_bp3)
 			cst3_name = "-parser:script_vars cst3=../cst3\n" ; flags_out.write(cst3_name)
-			
+
 			##### STEP 4 #####
                         blue = Blueprint('bp4')
 			blue.reindex_blueprint(start=1)
@@ -369,23 +371,23 @@ for comb in combinations:
 			st = "AtomPair N %i O %i HARMONIC 3.0 0.5\n" %(l6n+1,e4c-1); fileout.write(st) ; fileoutb.write(st)
 			st = "AtomPair N %i O %i HARMONIC 3.0 0.5\n" %(l6n+2,e4c-1); fileout.write(st) ; fileoutb.write(st)
 			#st = "AtomPair N %i O %i HARMONIC 3.0 0.5\n" %(e4c,l6n); fileout.write(st) ; fileoutb.write(st) #Additional Hbond between H3 C term and L8 N term
-			## H3-H4 distance from hairpin:     
+			## H3-H4 distance from hairpin:
                         e2 = blue.segment_dict['E2']
                         e2n = e2.bp_data[0][0]
                         l5 = blue.segment_dict['L5']
                         l5n = int(l5.bp_data[0][0])
-			
+
 			# E1-E2 Hairpin constraints
 			st = HbondsRegularHairpin(strand1="E1",strand2="E2",blueprint=blue) ; fileout.write(st); fileoutb.write(st);
-			
+
 			# Perfect Helices
 			st = PerfectHelixCst('bp4',1); fileoutb.write(st); fileout.write(st);
 			st = PerfectHelixCst('bp4',2); fileoutb.write(st); fileout.write(st);
-			
+
 			# Angle between E4 C, L6_2 and H3C to avoid weird loop
 			st = AngleConstraints(e4c-1,l6n+1,h3c,95,3,"E6H4") ; fileout.write(st); fileoutb.write(st);
 			st = AngleConstraints(e4c-1,l6n+1,h3c,95,15,"E6H4b") ; fileoutc.write(st); fileoutb.write(st);
-			
+
 			#Enforce ABEGO at positions 10 and 9:
 			l2 = blue.segment_dict['L2']
 			l2N = int(l2.bp_data[0][0])
@@ -395,9 +397,9 @@ for comb in combinations:
 			fileout.close()
 			fileoutb.close()
 			fileoutc.close()
-			
+
 			# Vars for Stage #4
-			SS_s4 = "-parser:script_vars s4_SS=%s\n" %(subprocess.check_output([ "bash", "/work/basantab/scripts/ss_from_bp.sh", "bp4"])) ; flags_out.write(SS_s4)
+			SS_s4 = "-parser:script_vars s4_SS=%s\n" %(''.join([ pos[2][0] for pos in Blueprint('bp4').bp_data])) ; flags_out.write(SS_s4)
 			#bp4_name = "-parser:script_vars bp4=../bp4\n" ; flags_out.write(bp4_name)
 			bp4b_name = "-parser:script_vars bp4.b=../bp4.b\n" ; flags_out.write(bp4b_name)
 			bp4 = Blueprint('bp4'); r_res = [ i+1 for i,pos in enumerate(bp4.bp_data) if pos[-1] == 'R' ]
@@ -408,7 +410,7 @@ for comb in combinations:
 			H34dist_r1 = "-parser:script_vars H34dist_r1=%s\n"%(str(e2n)) ; flags_out.write(H34dist_r1)
 			H34dist_r2 = "-parser:script_vars H34dist_r2=%s\n"%(str(l5n)) ; flags_out.write(H34dist_r2)
 			H34dist_dist = "-parser:script_vars H34dist_dist=%s\n"%("18") ; flags_out.write(H34dist_dist)
-			
+
 			#### STEP 5: Adding the C term Helix:
                         #### Distance between bulge2 and helix2
 			'''
@@ -433,17 +435,17 @@ for comb in combinations:
                         st = PairConstraints(H4c,H3c,6,2,"H4H3") ; fileout.write(st);
 			st = PairConstraints(H4c,H3c,6,2.5,"H4H3b") ; fileoutb.write(st);
 			st = PairConstraints(H4c-1,E4c,6,2,"H4E4a") ; fileout.write(st);
-			st = PairConstraints(H4c-1,E4c,6,2.5,"H4Eb") ; fileoutb.write(st); 
+			st = PairConstraints(H4c-1,E4c,6,2.5,"H4Eb") ; fileoutb.write(st);
 			st = PairConstraints(H4c-1,E4c+3,6,2,"H4E5") ; fileout.write(st);
 			st = PairConstraints(H4c-1,E4c+3,6,2.5,"H4E5b") ; fileoutb.write(st);
 			#st = AngleConstraints(E6c-2,L10,H4c,80,10,"E6H4") ; fileout.write(st); fileoutb.write(st); # Changed angle form 80 to 110
 			## Hbonds of linker-bulge between E6 and H5
-			
+
 			E5 = blue.segment_dict['E5']
 			E5n = int(E5.bp_data[0][0])
 			st = CircularHBondConstraints(L10,E5n+5) ; fileout.write(st)
                         st = CircularHBondConstraints(L10+1,E5n+5) ; fileout.write(st)
-			
+
 			fileout.close()
 			fileoutb.close()
 
@@ -459,7 +461,7 @@ for comb in combinations:
 			distH4Sheet_r2 = "-parser:script_vars distH4Sheet_r2=%s\n"%(str(E4c)) ; flags_out.write(distH4Sheet_r2)
 			distH4Sheet_dist = "-parser:script_vars distH4Sheet_dist=8.5\n" ; flags_out.write(distH4Sheet_dist)
 			'''
-			
+
                         ##### STEP 6 #####
                         ## Distance between bulge2 and helix2
                         blue = Blueprint('bp6') ; blue.reindex_blueprint(start=1)
@@ -487,11 +489,11 @@ for comb in combinations:
                         XMLReplaceXXXYYY(xml_lines=xml_lines,identifier='dist6d',xxx=h3n, yyy=h2c+3)
                         st = CircularHBondConstraints(h2c+5,h2c+2) ; fileout.write(st) ; fileoutb.write(st)
 			XMLReplaceXXXYYY(xml_lines=xml_lines,identifier='dist6e',xxx=h2c+5, yyy=h2c+2)
-			
+
 			## Perfect helices:
 			st = PerfectHelixCst('bp6',1); fileout.write(st); fileoutb.write(st);
                         st = PerfectHelixCst('bp6',2); fileout.write(st); fileoutb.write(st);
-			
+
 			## H1 csts:
 			h2n = int(seg1.bp_data[0][0])
 			h2c = int(seg1.bp_data[-1][0])
@@ -502,21 +504,25 @@ for comb in combinations:
 			e3 = blue.segment_dict['E3']
 			e3c = int(e3.bp_data[-1][0])
 			loop6 = int(blue.segment_dict['L6'].bp_data[0][0])
-			
-			st = PairConstraints(h1c-2,loop6,8,3,"H1L6a") ; fileout.write(st); fileoutb.write(st); fileoutc.write(st);
-			st = PairConstraints(h1c,loop6,8,3,"H1L6b") ; fileout.write(st); fileoutb.write(st);
+
+			st = PairConstraints(h1c-2,loop6,8,3,"H1L6a") ; fileoutc.write(st);
+			st = HarmonicPairConstraints(h1c-2,loop6,8,8,) ; fileout.write(st); fileoutb.write(st);
+			st = HarmonicPairConstraints(h1c,loop6,8,8,) ; fileout.write(st); fileoutb.write(st);
 			#st = PairConstraints(h1c-2,loop6,8,3,"H1L6c") ; fileout.write(st); fileoutb.write(st);
 			st = PairConstraints(h1n,e3c,8,3,"H1E3n") ; fileout.write(st); fileoutb.write(st);
-			st = PairConstraints(h1n,e3c+3,8,2,"H1E3") ; fileout.write(st); fileoutb.write(st); fileoutc.write(st);
-			st = PairConstraints(h1c-1,loop6+2,7.7,1.5,"H1L6b") ; fileout.write(st); fileoutb.write(st); fileoutc.write(st);
-			st = AngleConstraints(h1c-6,h1c+1,h2c,30,8,"E6H4") ; fileout.write(st); fileoutb.write(st); fileoutc.write(st);
-			
+			st = PairConstraints(h1n,e3c+3,8,2,"H1E3") ; fileoutc.write(st);
+			st = PairConstraints(h1c-1,loop6+2,7.7,1.5,"H1L6b") ; fileoutc.write(st);
+			st = AngleConstraints(h1c-6,h1c+1,h2c,30,8,"E6H4") ; fileoutc.write(st);
+			st = HarmonicPairConstraints(h1n,e3c+3,8,8) ; fileout.write(st); fileoutb.write(st);
+			st = HarmonicPairConstraints(h1c-1,loop6+2,7.7,8) ; fileout.write(st); fileoutb.write(st);
+			st = HarmonicAngleConstraints(h1c-6,h1c+1,h2c,30,30) ; fileout.write(st); fileoutb.write(st);
+
 			#H1-capping Hbond
 			LOOP2 = blue.segment_dict['L2']
 			l2n = int(LOOP2.bp_data[0][0])
 
 			st = "AtomPair N %i O %i HARMONIC 2.5 0.3\n" %(l2n,h1c-2) ; fileout.write(st) ; fileoutb.write(st)
-			
+
 			#Enforce ABEGO at positions 27 and 28:
                         l3 = blue.segment_dict['L3']
                         l3N = int(l3.bp_data[0][0])
@@ -527,9 +533,9 @@ for comb in combinations:
                         fileout.close()
 			fileoutb.close()
 			fileoutc.close()
-			
+
 			# Vars for Stage #6
-			SS_s6 = "-parser:script_vars s6_SS=%s\n" %(subprocess.check_output([ "bash", "/work/basantab/scripts/ss_from_bp.sh", "bp6"])) ; flags_out.write(SS_s6)
+			SS_s6 = "-parser:script_vars s6_SS=%s\n" %(''.join([ pos[2][0] for pos in Blueprint('bp6').bp_data])) ; flags_out.write(SS_s6)
                         bp6b_name = "-parser:script_vars bp6.b=../bp6.b\n" ; flags_out.write(bp6b_name)
                         bp6 = Blueprint('bp6'); r_res = [ i+1 for i,pos in enumerate(bp6.bp_data) if pos[-1] == 'R' ]
                         R_in_bp6= "-parser:script_vars R_in_bp6=%d-%d\n"%(r_res[0],r_res[-1]) ; flags_out.write(R_in_bp6)
@@ -548,12 +554,9 @@ for comb in combinations:
 			H1C_r1 = "-parser:script_vars H1C_r1=%s\n"%(str(h1c)) ; flags_out.write(H1C_r1)
 			H1C_r2 = "-parser:script_vars H1C_r2=%s\n"%(str(loop6)) ;flags_out.write(H1C_r2)
 			H1C_dist = "-parser:script_vars H1C_dist=%s\n"%("11.0") ;flags_out.write(H1C_dist)
-			'''
-			<AtomicDistance name="H1Na" residue1="%%H1Na_r1%%" atomname1="CA" residue2="%%H1Na_r2%%" atomname2="CA" distance="%%H1Na_dist%%" confidence="1" />
-			<AtomicDistance name="H1Nb" residue1="%%H1Nb_r1%%" atomname1="CA" residue2="%%H1Nb_r2%%" atomname2="CA" distance="%%H1Nb_dist%%" confidence="1" />
-			<AtomicDistance name="H2N" residue1="%%H2N_r1%%" atomname1="CA" residue2="%%H2N_r2%%" atomname2="CA" distance="%%H2N_dist%%" confidence="0" />
-			<AtomicDistance name="H1C" residue1="%%H1C_r1%%" atomname1="CA" residue2="%%H1C_r2%%" atomname2="CA" distance="%%H1C_dist%%" confidence="1" />
-			'''
+			flags_out.write('-restore_talaris_behavior\n')
+			flags_out.write("-picking_old_max_score 1\n")
+			flags_out.write("-rama_map ../../../Rama_XPG_3level.txt\n")
 			flags_out.close()
 			#-----------------------
 			# Write modified xml
@@ -562,7 +565,5 @@ for comb in combinations:
 			for line in xml_lines:
 				xml_out.write(line)
 			xml_out.close()
-			
+
 			os.chdir('../')
-
-
